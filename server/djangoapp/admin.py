c0887d866ad2e5f3fce32709d5f8efade3391777
Superior-Spidey <prithviscbpr@gmail.com>
2024-05-04 05:50:49 -0400

    POsting reviews

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,16 +1,38 @@
+# from django.contrib import admin
+# from .models import CarMake, CarModel
+
+# # Register your models here.
+# #admin.site.register(CarMake)
+# #admin.site.register(CarModel)
+
+# # CarModelInline class
+# class CarModelInline(admin.StackedInline):
+#     model = CarModel
+#     extra = 5
+# # CarModelAdmin class
+# class CarModelAdmin(admin.ModelAdmin):
+#     fields = ['make', 'name', 'type','year']
+#     inlines = [CarModelInline]
+# # CarMakeAdmin class with CarModelInline
+# class CarMakeAdmin(admin.ModelAdmin):
+#     fields = ['name', 'description']
+# # Register models here
+# admin.site.register(CarMake, CarMakeAdmin)
+# admin.site.register(CarModel, CarModelAdmin)
+
 from django.contrib import admin
-# from .models import related models
 from .models import CarMake, CarModel
 
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 0  # Change to 0 if you don't want to show extra empty forms
 
-# Register your models here.
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    inlines = [CarModelInline]
 
-# CarModelInline class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['make', 'name', 'type', 'year']
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
